2fb0e53eda192b1294047ef03293dfd2c82c78fa
dOJer113 <aleksandrroznov484@gmail.com>
2023-02-17 20:42:22 +0300

    mediumCropped

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = new int[]{1,10,0};
+        int[] sales = new int[]{1,10,0,100,200,39,44};
         SalesManager manager = new SalesManager(sales);
         System.out.println(manager.max());
+        System.out.println(manager.mediumCropped());
     }
 }
